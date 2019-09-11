2d5ecae23249c136978d47099c378384fae273cb
Victor <victorhvcunha@gmail.com>
2019-09-11 14:13:00 -0300

    Adicionado ListView

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -3,48 +3,53 @@ import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> _userTransactions;
 
   TransactionList(this._userTransactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _userTransactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, id) {
+            return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${_userTransactions[id].amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat('MMMM dd, yyyy').format(tx.date),
-                    style: TextStyle(color: Colors.black38),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _userTransactions[id].title,
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat('MMMM dd, yyyy').format(_userTransactions[id].date),
+                      style: TextStyle(color: Colors.black38),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: _userTransactions.length,
+       
+      ),
     );
   }
 }
