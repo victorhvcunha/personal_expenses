1123c908e23edfcbccd8ffa14b8503efd25fb2b3
Victor <victorhvcunha@gmail.com>
2019-09-14 15:51:24 -0300

    ajustado visualização em % da tela

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -10,56 +10,54 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: _userTransactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transactions added yet!',
-                    style: Theme.of(context).textTheme.title),
-                SizedBox(height: 10),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/img/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
+    return _userTransactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text('No transactions added yet!',
+                  style: Theme.of(context).textTheme.title),
+              SizedBox(height: 10),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/img/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  child: ListTile(
-                    leading: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: FittedBox(
-                        child: CircleAvatar(
-                          radius: 40,
-                          child: Text(
-                            '\$${_userTransactions[index].amount.toStringAsFixed(2)}',
-                          ),
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                child: ListTile(
+                  leading: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FittedBox(
+                      child: CircleAvatar(
+                        radius: 40,
+                        child: Text(
+                          '\$${_userTransactions[index].amount.toStringAsFixed(2)}',
                         ),
                       ),
                     ),
-                    title: Text(
-                      _userTransactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(_userTransactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => _deleteTransaction(_userTransactions[index].id),
-                    ),
                   ),
-                );
-              },
-              itemCount: _userTransactions.length,
-            ),
-    );
+                  title: Text(
+                    _userTransactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(_userTransactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () =>
+                        _deleteTransaction(_userTransactions[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: _userTransactions.length,
+          );
   }
 }
